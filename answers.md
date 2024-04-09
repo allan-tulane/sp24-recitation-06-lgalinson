481c89d52bb0e69efeba90950552d91a3b86d28a
Lakeland Galinson <lakeland@galinson.com>
2024-04-09 13:33:35 -0500

    Final Commit

--- answers.md
+++ answers.md
@@ -1,7 +1,8 @@
 # CMPS 2200 Recitation 06
 ## Answers
 
-**Name:**_________________________
+**Name:** Lakeland Galinson
+
 **Name:**_________________________
 
 
@@ -10,11 +11,15 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 
 - **2)**
+$W(n)=2+W(n−1)+W(n−2)$, and the total work is between $O(2^n)$ and $O(\sqrt2^n)$
 
 - **3)**
+$S(n-1) + c$, and the Span is $O(n)$
 
 - **4)**
-
+The "count" array shows how many time each Fibonacci number is computed. The pattern demonstrates that there is much duplicate work being done in the computation. 
 - **6)**
+The maximum number of times it will be called is 1 time. The span and work are both $O(n)$.
 
 - **8)**
+The maximum times F_i will get read is 2 times. The work and span for the bottom-up function would be $O(n)$
--- main.py
+++ main.py
@@ -4,21 +4,46 @@ def fib_recursive(n, counts):
     counts is a list of n+1 elements, where counts[i] is incremented
     each time fib_recursive(i, counts) is called.
     """    
+    # base cases
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    
     counts[n] += 1
-    ###TODO
-    pass
+    
+    # recursion
+    return fib_recursive(n-1, counts) + fib_recursive(n-2, counts)
     
 
     
 def fib_top_down(n, fibs):
-    ###TODO
-    pass
-
+    # check if F(n) has already been computed
+    if fibs[n] != -1:
+        return fibs[n]
+    
+    # base cases
+    if n == 0:
+        fibs[0] = 0
+    elif n == 1:
+        fibs[1] = 1
+    else:
+        # recursive calls
+        fibs[n] = fib_top_down(n-1, fibs) + fib_top_down(n-2, fibs)
+    
+    return fibs[n] 
 
 def fib_bottom_up(n):
-    ###TODO
-    pass
-
+    if n <= 1:
+        return n
+    
+    fibs = [0] * (n + 1)
+    fibs[1] = 1 
+    
+    for i in range(2, n + 1):
+        fibs[i] = fibs[i - 1] + fibs[i - 2]
+    
+    return fibs[n]
 
 
 
